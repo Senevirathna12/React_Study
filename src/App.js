2934b2e42d549f7c9744a8832ed2b87b51e419f8
Senevirathna12 <139707941+Senevirathna12@users.noreply.github.com>
2024-11-25 14:51:08 +0530

    Use effect explenation done

--- src/App.js
+++ src/App.js
@@ -1,70 +1,57 @@
-import { Fragment, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Unit from "./Components/Unit";
-import Input from "./Components/Input";
-import Header from "./Components/Header";
 
-function App() {
-  const [inputData, setInputData] = useState({
-    imageUrl: "",
-    name: "",
-    city: "",
-    position: "",
-  });
-  const [myData, setMydata] = useState([]);
+const App = () => {
+  const [apiId, setApiId] = useState("1");
+  const [data ,setData] =useState({});
+  const [search ,setSerch] = useState(0);
+
+  // console.log(apiId);
+  // console.log(data);
+
+  useEffect(()=>{
+    console.log('use effect running..');
+    const apiCall = async ()=>{
+      console.log('function api call running')
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${apiId}`);
+      const data = await res.json();
+      if(data){
+        setData(data);
+      }
+    }
+
+    if(apiId.length > 0 && Number(apiId) >0 && Number(apiId) <= 100){
+      console.log('if condition running..')
+      apiCall();
+    }
+
+    // cleanup function
+    return () => {
+      console.log('cleanUp')
+      apiCall();
+    }
+    // eslint-disable-next-line
+  },[search]);
 
   return (
-    <Fragment>
-      <Header/>
-      <div className="main__container">
-        <div className="main_left">
-          <Input 
-            imageUrl={inputData.imageUrl} 
-            name={inputData.name}
-            city={inputData.city}
-            position={inputData.position}
-            setInputData={setInputData} 
-          />
+    <div>
+      <input
+        type="text"
+        placeholder="Enter id"
+        value={apiId}
+        onChange={(e) => {
+          setApiId(e.target.value);
+        }}
+      />
+      <button onClick={()=> setSerch(pre => pre === 0 ? 1 : 0)}>Search</button>
+      {data && <div>
+        <h2>{data.title}</h2>
+        <p>{data.body}</p>
+      </div>}
 
-          <button
-            onClick={() => {
-              setMydata((pre) => [
-                ...pre,
-                {
-                  image: inputData.imageUrl,
-                  name: inputData.name,
-                  city: inputData.city,
-                  position: inputData.position,
-                },
-              ]);
 
-              setInputData({
-                imageUrl: "",
-                name: "",
-                city: "",
-                position: "",
-              });
-            }}
-          >
-            Submit
-          </button>
-        </div>
-        <div className="main_right">
-          {myData?.map(({ image, name, city, position }, index) => {
-            return (
-              <Unit
-                image={image}
-                name={name}
-                city={city}
-                position={position}
-                key={index}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </Fragment>
+    </div>
   );
-}
+};
 
 export default App;
